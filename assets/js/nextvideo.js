8fa9940eb1e7032ec00571fcf30bd730b694b071
RiskyVenture <65242086+RiskyVenture@users.noreply.github.com>
2020-06-28 23:50:17 +0100

    The Ade Update
    While current_video == next_video {

--- assets/js/nextvideo.js
+++ assets/js/nextvideo.js
@@ -1,10 +1,23 @@
+
+
+var current_video=0;
+
 function newVid() {
-    var vidEmbeds = ['https://www.youtube.com/embed/NZXp1tcvAhs?controls=0;autoplay=1',
-      'https://www.youtube.com/embed/izTaPcZZAXs?controls=0;autoplay=1',
-      'https://www.youtube.com/embed/i2YJF4Yhp1I?rel=0;controls=0;autoplay=1'
+    var vidEmbeds = ['https://www.youtube.com/embed/NZXp1tcvAhs?controls=0;autoplay=1;playsinline=1',
+      'https://www.youtube.com/embed/izTaPcZZAXs?controls=0;autoplay=1;playsinline=1',
+      'https://www.youtube.com/embed/i2YJF4Yhp1I?rel=0;controls=0;autoplay=1;playsinline=1'
     ];
-  
-    document.getElementById('iframemain').src = vidEmbeds[Math.random() * vidEmbeds.length| 0];
+    var next_video = Math.random() * vidEmbeds.length|0;
+
+    while (current_video == next_video ) {
+        next_video = Math.random() * vidEmbeds.length| 0;
+    }
+
+    current_video = next_video;
+
+    document.getElementById('iframemain').src = vidEmbeds[current_video];
   }
   
+
+
 // see for explanation of | 0]   https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
